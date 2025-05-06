820ac2c6691ea388b7d3d1c241c9cf6ddaf29e65
Jane Panov <panovjane@gmail.com>
2025-05-06 23:08:45 +0200

    feat: initialize project, configure application.properties

---
+++ src/main/java/mk/ukim/finki/testcraftai/TestCraftAiApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.testcraftai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCraftAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCraftAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/testcraftai/TestCraftAiApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.testcraftai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCraftAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
