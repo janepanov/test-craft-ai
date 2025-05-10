08016c54ba84cf9158630ac46f68893ff90690f2
Jane Panov <panovjane@gmail.com>
2025-05-10 16:40:13 +0200

    feat(repository): add subject repository

---
+++ src/main/java/mk/ukim/finki/testcraftai/repository/SubjectRepository.java
@@ -0,0 +1,10 @@
+package mk.ukim.finki.testcraftai.repository;
+
+import mk.ukim.finki.testcraftai.model.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+    boolean existsByName(String name);
+}
